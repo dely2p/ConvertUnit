6ae76261e733ad3d7e4d06d6eba9aa7f863c17fa
dely2p <lhyal2p@gmail.com>
2017-10-16 16:21:41 +0900

    convert unit _cm to m_m to cm

---
+++ ChangeUnit.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var centimeter : Int = 120
+var centimeter_to_cm = Double(centimeter)/100
+print(centimeter, "cm를 ",centimeter_to_cm, "로 출력한다.")
+
+var meter : Double = 1.86
+var meter_to_cm = Int(meter*100)
+print(meter, "m를 ",meter_to_cm, "cm로 출력한다.")
