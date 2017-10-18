c9e2069b181e6a253a672b46bb3d40c61ef323f2
dely2p <lhyal2p@gmail.com>
2017-10-18 14:39:11 +0900

    add yard converter

--- UnitConverter/main.swift
+++ UnitConverter/main.swift
@@ -11,8 +11,10 @@ import Foundation
  let Unit : [String] = [] //["inch","cm","m"]
  let UnitDictionary : [String : Unit] = ["inch" : [2.54, 0.0254, 0]]
  */
-let Unit = ["inch","cm","m"]
+let Unit = ["inch","cm","m","yard"]
 let calc_val = 100.0
+var inputValue : String? = nil
+var PLAY = true
 
 func SeperateUnit (value: String) -> (num: Double,unit: String) {
     var unit : Substring = ""
@@ -57,7 +59,18 @@ func Inch_to_M(num: Double) -> Double{
 func M_to_Inch(num: Double) -> Double{
     return Cm_to_Inch(num: M_to_Cm(num: num))
 }
-
+func Yard_to_Cm(num: Double) -> Double{
+    return num*91.44
+}
+func Yard_to_Inch(num: Double) -> Double{
+    return Cm_to_M(num: Yard_to_Cm(num: num))
+}
+func Cm_to_Yard(num: Double) -> Double{
+    return num*0.010936
+}
+func M_to_Yard(num: Double) -> Double {
+    return Cm_to_Yard(num: M_to_Cm(num: num))
+}
 func UnitConvertor (num: Double, inputUnit: String, convertUnit: String) -> (num: Double, convertUnit: String) {
     var convtNum = 0.0
     var convtUnit = ""
@@ -79,7 +92,15 @@ func UnitConvertor (num: Double, inputUnit: String, convertUnit: String) -> (num
     } else if(inputUnit=="m" && convertUnit=="inch"){
         convtNum = M_to_Inch(num: num)
         convtUnit = convertUnit
-    } else if(!Unit.contains(inputUnit) || !Unit.contains(convertUnit)){
+    } else if(inputUnit=="yard" && (convertUnit=="m" || convertUnit==" ")){
+        convtNum = Yard_to_Inch(num: num)
+        convtUnit = "m"
+    } else if(inputUnit=="m" && convertUnit=="yard"){
+        convtNum = M_to_Yard(num: num)
+        convtUnit = convertUnit
+    }
+    
+    else if(!Unit.contains(inputUnit) || !Unit.contains(convertUnit)){
         print("지원하지 않는 단위입니다.")
         return (0.0,"")
     }
@@ -98,10 +119,15 @@ func Convert (str: String) -> String {
     return String(result.num)+result.convertUnit
 }
 
-print("값 입력: ")
-if let inputValue = readLine(){
-    let result = Convert(str: inputValue)
-    print("\(result)","")
-}
-
+repeat{
+    print("값 입력: ")
+    inputValue = readLine()
+    //print(inputValue!)
+    if inputValue == "q" || inputValue == "quit" {
+        PLAY = false
+    }else if inputValue != nil {
+        let result = Convert(str: inputValue!)
+        print("\(result)","")
+    }
+}while PLAY
 
