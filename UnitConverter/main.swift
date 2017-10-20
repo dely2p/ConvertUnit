c66a2a27abe151dd6ee691c0d4713407dd29aaa9
dely2p <lhyal2p@gmail.com>
2017-10-20 11:51:21 +0900

    used extension

--- UnitConverter/main.swift
+++ UnitConverter/main.swift
@@ -7,22 +7,30 @@
 //
 
 import Foundation
-/*
- let Unit : [String] = [] //["inch","cm","m"]
- let UnitDictionary : [String : Unit] = ["inch" : [2.54, 0.0254, 0]]
- */
-let Unit = ["inch","cm","m","yard"]
-let calc_val = 100.0
-var inputValue : String? = nil
-var PLAY = true
 
-func SeperateUnit (value: String) -> (num: Double,unit: String) {
+var PLAY = true
+let Units = ["inch","cm","m","yard"]
+extension Double {
+    var cmToM: Double { return self/100 }
+    var cmToinch: Double { return self.cmToM.mToInch }
+    var cmToYard: Double { return self*0.010936}
+    var mToCm: Double { return self*100 }
+    var mToInch: Double { return self*39.370079 }
+    var mToYard: Double { return self.mToCm.cmToYard }
+    var inchTocm: Double { return self.inchToM.mToCm }
+    var inchToM: Double { return self*0.0254 }
+    var yardToCm: Double { return self*91.44}
+    var yardToM: Double { return self.yardToCm.cmToM }
+}
+
+// seperate inputvalue to number and unit
+func SeperateNumber_Unit (value: String) -> (num: Double,unit: String) {
     var unit : Substring = ""
     var num : Double? = nil
-    let unit_num = Unit.count
-    for i in 0..<unit_num {
-        if value.contains(Unit[i]){
-            let unitCnt = Unit[i].count
+    let unit_size = Units.count
+    for i in 0..<unit_size {
+        if value.contains(Units[i]){
+            let unitCnt = Units[i].count
             let sep = value.index(value.endIndex,offsetBy:(-unitCnt))
             
             num = Double(value[value.startIndex..<sep])
@@ -32,7 +40,9 @@ func SeperateUnit (value: String) -> (num: Double,unit: String) {
     }
     return (num!,String(unit))
 }
-func GetVal_ConvertVal(value: String)-> (inputVal: String,convertUnit: String){
+
+// seperate inputvalue and convert unit
+func SeperateInputVal_ConvertUnit(value: String)-> (inputVal: String,convertUnit: String){
     let tmp = value.split(separator: " ")
     let inputVal = String(tmp[0])
     var convertUnit = " "
@@ -41,75 +51,35 @@ func GetVal_ConvertVal(value: String)-> (inputVal: String,convertUnit: String){
     }
     return (inputVal, convertUnit)
 }
-func Cm_to_M(num: Double) -> Double{
-    return num*0.01
-}
-func M_to_Cm(num: Double) -> Double{
-    return num*100
-}
-func Cm_to_Inch(num: Double) -> Double{
-    return num*0.393701
-}
-func Inch_to_Cm(num: Double) -> Double{
-    return num*2.54
-}
-func Inch_to_M(num: Double) -> Double{
-    return Cm_to_M(num: Inch_to_Cm(num: num))
-}
-func M_to_Inch(num: Double) -> Double{
-    return Cm_to_Inch(num: M_to_Cm(num: num))
-}
-func Yard_to_Cm(num: Double) -> Double{
-    return num*91.44
-}
-func Yard_to_Inch(num: Double) -> Double{
-    return Cm_to_M(num: Yard_to_Cm(num: num))
-}
-func Cm_to_Yard(num: Double) -> Double{
-    return num*0.010936
-}
-func M_to_Yard(num: Double) -> Double {
-    return Cm_to_Yard(num: M_to_Cm(num: num))
-}
+
+// convert each unit
 func UnitConvertor (num: Double, inputUnit: String, convertUnit: String) -> (num: Double, convertUnit: String) {
     var convtNum = 0.0
-    var convtUnit = ""
-    if(inputUnit=="cm" && (convertUnit=="m" || convertUnit==" ")){
-        convtNum = Cm_to_M(num: num)
-        convtUnit = "m"
-    } else if(inputUnit=="m" && (convertUnit=="cm" || convertUnit==" ")){
-        convtNum = M_to_Cm(num: num)
-        convtUnit = "cm"
-    } else if(inputUnit=="cm" && convertUnit=="inch"){
-        convtNum = Cm_to_Inch(num: num)
-        convtUnit = convertUnit
-    } else if(inputUnit=="inch" && convertUnit=="cm"){
-        convtNum = Inch_to_Cm(num: num)
-        convtUnit = convertUnit
-    } else if(inputUnit=="inch" && convertUnit=="m"){
-        convtNum = Inch_to_M(num: num)
-        convtUnit = convertUnit
-    } else if(inputUnit=="m" && convertUnit=="inch"){
-        convtNum = M_to_Inch(num: num)
-        convtUnit = convertUnit
-    } else if(inputUnit=="yard" && (convertUnit=="m" || convertUnit==" ")){
-        convtNum = Yard_to_Inch(num: num)
-        convtUnit = "m"
-    } else if(inputUnit=="m" && convertUnit=="yard"){
-        convtNum = M_to_Yard(num: num)
-        convtUnit = convertUnit
-    }
+    var convtUnit = convertUnit
     
-    else if(!Unit.contains(inputUnit) || !Unit.contains(convertUnit)){
-        print("지원하지 않는 단위입니다.")
-        return (0.0,"")
+    //inputUnit -> convertUnit
+    switch (inputUnit,convertUnit){
+        case ("cm","m"): convtNum = num.cmToM
+        case ("cm"," "): convtNum = num.cmToM; convtUnit = "m"
+        case ("m","cm"): convtNum = num.mToCm
+        case ("m", " "): convtNum = num.mToCm; convtUnit = "cm"
+        case ("cm","inch"): convtNum = num.cmToinch
+        case ("inch","cm"): convtNum = num.inchTocm
+        case ("m","inch"): convtNum = num.mToInch
+        case ("inch","m"): convtNum = num.inchToM
+        case ("yard","m"): convtNum = num.yardToM
+        case ("yard"," "): convtNum = num.yardToM; convtUnit = "m"
+        case ("m","yard"): convtNum = num.mToYard
+        default:
+            print("지원하지 않는 단위입니다.")
     }
     return (convtNum,convtUnit)
 }
+
+// operate func
 func Convert (str: String) -> String {
-    //입력값 2개 받는 함수(공백으로 구분)
-    let inputval = GetVal_ConvertVal(value: "\(str)")
-    let sepValue = SeperateUnit(value: "\(inputval.inputVal)")
+    let inputval = SeperateInputVal_ConvertUnit(value: "\(str)")
+    let sepValue = SeperateNumber_Unit(value: "\(inputval.inputVal)")
     let convertUnit = inputval.convertUnit
     
     let result = UnitConvertor(num: sepValue.num, inputUnit: sepValue.unit, convertUnit: convertUnit)
@@ -119,15 +89,20 @@ func Convert (str: String) -> String {
     return String(result.num)+result.convertUnit
 }
 
-repeat{
-    print("값 입력: ")
-    inputValue = readLine()
-    //print(inputValue!)
-    if inputValue == "q" || inputValue == "quit" {
-        PLAY = false
-    }else if inputValue != nil {
-        let result = Convert(str: inputValue!)
-        print("\(result)","")
-    }
-}while PLAY
+func Prompt() {
+    var inputValue : String? = ""
+    repeat{
+        print("값 입력: ")
+        inputValue = readLine()
+    
+        if inputValue == "q" || inputValue == "quit" {
+            PLAY = false
+        }else if inputValue != nil {
+            let result = Convert(str: inputValue!)
+            print("\(result)","")
+        }
+    }while PLAY
+}
+
+Prompt()
 
