084cb3f80e26862e5efcf9bb6d911d66ea482e36
Elly <lhyal2p@gmail.com>
2017-10-24 13:59:04 +0900

    use Separate func and UnitConverter func

--- UnitConverter/main.swift
+++ UnitConverter/main.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 var PLAY = true
-let Units = ["inch", "cm", "m", "yard", "g", "kg", "lb", "oz", "l", "pt", "qt", "gal"]
+//let Units = ["inch", "cm", "m", "yard", "g", "kg", "lb", "oz", "l", "pt", "qt", "gal"]
 
 enum LengthConvertValue: Double {
     case CmToM = 0.01
@@ -99,84 +99,107 @@ class Volume {
         return value * VolumeConvertValue.QtToL.rawValue
     }
 }
-// seperate inputvalue to number and unit
-func seperateNumberUnit (value: String) -> (num: Double, unit: String) {
-    var cnt = 0
-    
-    for i in value {
-        if (i >= "a" && i <= "z") || (i >= "A" && i <= "Z") { //word
-            break;
-        }else {
-            cnt += 1;
+class Seperate {
+    // seperate inputvalue and convert unit(" ")
+    func seperateInputValConvertUnit(value: String)-> (inputVal: String, convertUnit: String){
+        let tmp = value.split(separator: " ")
+        let inputVal = String(tmp[0])
+        var convertUnit = " "
+        if value.contains(" ") {
+            convertUnit = String(tmp[1])
         }
+        return (inputVal, convertUnit)
+    }
+
+    // seperate inputvalue to number and unit(number, word)
+    func seperateNumberUnit (value: String) -> (num: Double, unit: String) {
+        var cnt = 0
+        for i in value {
+            if (i >= "a" && i <= "z") || (i >= "A" && i <= "Z") { //word
+                break;
+            }else {
+                cnt += 1;
+            }
+        }
+        let numIndex = value.index(value.startIndex, offsetBy: cnt)
+        let num = Double(value[value.startIndex..<numIndex])
+        let unit = value[numIndex..<value.endIndex]
+        
+        return (num!,String(unit))
     }
     
-    let numIndex = value.index(value.startIndex, offsetBy: cnt)
-    let num = Double(value[value.startIndex..<numIndex])
-    let unit = value[numIndex..<value.endIndex]
-
-    return (num!,String(unit))
-}
-
-// seperate inputvalue and convert unit
-func seperateInputValConvertUnit(value: String)-> (inputVal: String, convertUnit: String){
-    let tmp = value.split(separator: " ")
-    let inputVal = String(tmp[0])
-    var convertUnit = " "
-    if value.contains(" ") {
-        convertUnit = String(tmp[1])
+    // seperate units(,)
+    func seperateConvertUnit(value: String) -> Array<Substring> {
+        let Units = value.split(separator: ",")
+        return Units
     }
-    return (inputVal, convertUnit)
 }
 
 // convert each unit
-func unitConvertor(num: Double, inputUnit: String, convertUnit: String) -> (num: Double, convertUnit: String) {
-    var convtNum = 0.0
-    var convtUnit = convertUnit
-    var tmp = 0.0
-    
-    //inputUnit -> convertUnit
-    switch (inputUnit,convertUnit){
-    case ("cm","m"): convtNum = Length.cmToM(num)
-    case ("cm"," "): convtNum = Length.cmToM(num); convtUnit = "m"
-    case ("m","cm"): convtNum = Length.mToCm(num)
-    case ("m", " "): convtNum = Length.mToCm(num); convtUnit = "cm"
-    case ("cm","inch"): tmp = Length.cmToM(num); convtNum = Length.mToInch(tmp)
-    case ("inch","cm"): tmp = Length.inchToM(num); convtNum = Length.mToCm(tmp)
-    case ("m","inch"): convtNum = Length.mToInch(num)
-    case ("inch","m"): convtNum = Length.inchToM(num)
-    case ("yard","m"): tmp = Length.yardToCm(num); convtNum = Length.cmToM(tmp)
-    case ("yard"," "): tmp = Length.yardToCm(num); convtNum = Length.cmToM(tmp); convtUnit = "m"
-    case ("m","yard"): tmp = Length.mToCm(num); convtNum = Length.cmToYard(tmp)
+class UnitConverter {
+
+    //show every convert units
+    func noConvertUnit(num: Double, inputUnit: String) {
         
-    case ("g","kg"): convtNum = Weight.gTokg(num)
-    case ("kg","g"): convtNum = Weight.kgToG(num)
-    case ("kg","lb"): convtNum = Weight.kgToLb(num)
-    case ("kg","oz"): convtNum = Weight.kgToOz(num)
-    case ("lb","kg"): convtNum = Weight.lbToKg(num)
-    case ("oz","kg"): convtNum = Weight.ozToKg(num)
-        
-    case ("gal","l"): convtNum = Volume.galToL(num)
-    case ("l","gal"): convtNum = Volume.lToGal(num)
-    case ("l","pt"): convtNum = Volume.lToPT(num)
-    case ("l","qt"): convtNum = Volume.lToQt(num)
-    case ("pt","l"): convtNum = Volume.ptToL(num)
-    case ("qt","l"): convtNum = Volume.qtToL(num)
-        
-    default:
-        print("지원하지 않는 단위입니다.")
     }
-    
-    return (convtNum,convtUnit)
+    //show one convert unit
+    func oneConvertUnit(num: Double, inputUnit: String, convertUnit: String) -> (num: Double, convertUnit: String) {
+        var convtNum = 0.0
+        var convtUnit = convertUnit
+        var tmp = 0.0
+        
+        //inputUnit -> convertUnit
+        switch (inputUnit,convertUnit){
+        case ("cm","m"): convtNum = Length.cmToM(num)
+        //case ("cm"," "): convtNum = Length.cmToM(num); convtUnit = "m"
+        case ("m","cm"): convtNum = Length.mToCm(num)
+        //case ("m", " "): convtNum = Length.mToCm(num); convtUnit = "cm"
+        case ("cm","inch"): tmp = Length.cmToM(num); convtNum = Length.mToInch(tmp)
+        case ("inch","cm"): tmp = Length.inchToM(num); convtNum = Length.mToCm(tmp)
+        case ("m","inch"): convtNum = Length.mToInch(num)
+        case ("inch","m"): convtNum = Length.inchToM(num)
+        case ("yard","m"): tmp = Length.yardToCm(num); convtNum = Length.cmToM(tmp)
+        //case ("yard"," "): tmp = Length.yardToCm(num); convtNum = Length.cmToM(tmp); convtUnit = "m"
+        case ("m","yard"): tmp = Length.mToCm(num); convtNum = Length.cmToYard(tmp)
+            
+        case ("g","kg"): convtNum = Weight.gTokg(num)
+        case ("kg","g"): convtNum = Weight.kgToG(num)
+        case ("kg","lb"): convtNum = Weight.kgToLb(num)
+        case ("kg","oz"): convtNum = Weight.kgToOz(num)
+        case ("lb","kg"): convtNum = Weight.lbToKg(num)
+        case ("oz","kg"): convtNum = Weight.ozToKg(num)
+            
+        case ("gal","l"): convtNum = Volume.galToL(num)
+        case ("l","gal"): convtNum = Volume.lToGal(num)
+        case ("l","pt"): convtNum = Volume.lToPT(num)
+        case ("l","qt"): convtNum = Volume.lToQt(num)
+        case ("pt","l"): convtNum = Volume.ptToL(num)
+        case ("qt","l"): convtNum = Volume.qtToL(num)
+            
+        default:
+            print("지원하지 않는 단위입니다.")
+        }
+        
+        return (convtNum,convtUnit)
+
+    }
+    //show input each convert unit
+    func manyConvertUnit(num: Double, inputUnit: String, convertUnit: Array<String>){
+        
+    }
 }
 
 // operate func
 func convert (str: String) -> String {
-    let inputval = seperateInputValConvertUnit(value: "\(str)")
-    let sepValue = seperateNumberUnit(value: "\(inputval.inputVal)")
+    let inputWholeStr = Seperate()
+    let inputval = inputWholeStr.seperateInputValConvertUnit(value: "\(str)")
+    let sepValue = inputWholeStr.seperateNumberUnit(value: "\(inputval.inputVal)")
+    //let sepUnits = inputWholeStr.seperateConvertUnit(value: "\(inputval.convertUnit)")
+
     let convertUnit = inputval.convertUnit
     
-    let result = unitConvertor(num: sepValue.num, inputUnit: sepValue.unit, convertUnit: convertUnit)
+    let converter = UnitConverter()
+    let result = converter.oneConvertUnit(num: sepValue.num, inputUnit: sepValue.unit, convertUnit: convertUnit)
     if(result.num == 0.0 && result.convertUnit == ""){
         return ""
     }
